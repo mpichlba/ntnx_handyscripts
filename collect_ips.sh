01a92cc8161646a502fb9a4ceb9e30fae3ac48b1
Manfred Pichlbauer <m.pichlbauer@bacher.at>
2025-10-06 19:20:14 +0200

    Add informative echo statements for IP collection script

--- collect_ips.sh
+++ collect_ips.sh
@@ -2,4 +2,7 @@
 clusterip=$(ncli cluster info | awk -F: '/External IP/ {gsub(/^ /, "", $2); print $2}')
 svmips=$(for a in $(svmips); do echo -n "$a," ; done)
 allips=$(echo -n $svmips$clusterip)
+echo "Cluster and SVM IPs:"
 echo $allips
+echo "Now you can execute the following command to whitelist these IPs for enabling CCLM:"
+echo  allssh \"modify_firewall -f -r $allips -p 2030,2036,2073,2090 -i eth0\""
