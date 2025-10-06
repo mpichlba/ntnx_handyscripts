39bc0b7ad1378b85e39e619cc28261579cbb38eb
Manfred Pichlbauer <m.pichlbauer@bacher.at>
2025-10-06 19:25:24 +0200

    Fix syntax error in echo command for firewall modification

--- collect_ips.sh
+++ collect_ips.sh
@@ -5,4 +5,5 @@ allips=$(echo -n $svmips$clusterip)
 echo "Cluster and SVM IPs:"
 echo $allips
 echo "Now you can execute the following command to whitelist these IPs for enabling CCLM:"
-echo  allssh \"modify_firewall -f -r $allips -p 2030,2036,2073,2090 -i eth0\""
+echo  allssh \"modify_firewall -f -r $allips -p 2030,2036,2073,2090 -i eth0\"
+
