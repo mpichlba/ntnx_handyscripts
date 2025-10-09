fbaf64308d214878c512aecd6c14d569582c4639
Manfred Pichlbauer <m.pichlbauer@bacher.at>
2025-10-09 16:16:38 +0200

    Refine echo statements for clarity in IP whitelisting instructions

--- collect_ips.sh
+++ collect_ips.sh
@@ -8,6 +8,7 @@ svmips=$(for a in $(svmips); do echo -n "$a," ; done)
 allips=$(echo -n $svmips$clusterip)
 echo "Cluster and SVM IPs:"
 echo $allips
-echo "Now you can execute the following command to whitelist these IPs for enabling CCLM:"
+echo "Execute the following command to whitelist these IPs for enabling CCLM:"
 echo  allssh \"modify_firewall -f -r $allips -p 2030,2036,2073,2090 -i eth0\"
-
+echo "Execute the following command to whitelist these IPs for enabling scp access to the cluster:"
+echo  allssh \"modify_firewall -f -r $allips -p 2222 -i eth0\"
